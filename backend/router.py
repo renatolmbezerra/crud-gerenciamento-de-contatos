4a0b180c2d8a155fca1059b6ee7b9e263f353c83
Renato Macedo <renatolmb@hotmail.com>
2025-03-07 19:14:20 -0300

    Ajustes do arquivo router.py

--- backend/router.py
+++ backend/router.py
@@ -19,37 +19,37 @@ def health_check():
     return {"status": "ok"}
 
 @router.post("/contacts/", response_model=ContactResponse)
-def create_product_route(product: ContactCreate, db: Session = Depends(get_db)):
-    return create_contact(db=db, product=product)
+def create_contact_route(contact: ContactCreate, db: Session = Depends(get_db)):
+    return create_contact(db=db, contact=contact)
 
 
 @router.get("/contacts/", response_model=List[ContactResponse])
-def read_all_products_route(db: Session = Depends(get_db)):
-    products = get_contacts(db)
-    return products
+def read_all_contacts_route(db: Session = Depends(get_db)):
+    contacts = get_contacts(db)
+    return contacts
 
 
-@router.get("/contacts/{product_id}", response_model=ContactResponse)
-def read_product_route(product_id: int, db: Session = Depends(get_db)):
-    db_product = get_contact(db, product_id=product_id)
-    if db_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
-    return db_product
+@router.get("/contacts/{contact_id}", response_model=ContactResponse)
+def read_contact_route(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = get_contact(db, contact_id=contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
 
 
-@router.delete("/contacts/{product_id}", response_model=ContactResponse)
-def detele_product_route(product_id: int, db: Session = Depends(get_db)):
-    db_product = delete_contact(db, product_id=product_id)
-    if db_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
-    return db_product
+@router.delete("/contacts/{contact_id}", response_model=ContactResponse)
+def detele_contact_route(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = delete_contact(db, contact_id=contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
 
 
-@router.put("/contacts/{product_id}", response_model=ContactResponse)
-def update_product_route(
-    product_id: int, product: ContactUpdate, db: Session = Depends(get_db)
+@router.put("/contacts/{contact_id}", response_model=ContactResponse)
+def update_contact_route(
+    contact_id: int, contact: ContactUpdate, db: Session = Depends(get_db)
 ):
-    db_product = update_contact(db, product_id=product_id, product=product)
-    if db_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
-    return db_product
+    db_contact = update_contact(db, contact_id=contact_id, contact=contact)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
