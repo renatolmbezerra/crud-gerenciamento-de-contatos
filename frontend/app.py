8ef4d7226716ff20de06826808c3aeabeb01c036
Renato Macedo <renatolmb@hotmail.com>
2025-03-09 16:38:22 -0300

    Frontend finalizado

--- frontend/app.py
+++ frontend/app.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 
 st.image("logo.png", width=200)
 
-st.title("Gerenciamento de Produtos")
+st.title("Gerenciamento de Contatos")
 
 
 # Função auxiliar para exibir mensagens de erro detalhadas
@@ -28,47 +28,60 @@ def show_response_message(response):
             st.error("Erro desconhecido. Não foi possível decodificar a resposta.")
 
 
-# Adicionar Produto
-with st.expander("Adicionar um Novo Produto"):
-    with st.form("new_product"):
-        name = st.text_input("Nome do Produto")
-        description = st.text_area("Descrição do Produto")
-        price = st.number_input("Preço", min_value=0.01, format="%f")
-        categoria = st.selectbox(
-            "Categoria",
-            ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
-        )
-        email_fornecedor = st.text_input("Email do Fornecedor")
+# Adicionar Contato
+with st.expander("Adicionar um Novo Contato"):
+    with st.form("new_contact"):
+        operador = st.selectbox("Operador", ["Matheus", "Lucas", "João", "Pedro", "Maria", "Ana", "José", "Carlos"])
+        dataContato = st.date_input("Data do Contato")
+        nomeCliente = st.text_input("Nome do Cliente")
+        pessoaContato = st.text_input("Pessoa de Contato")
+        formaContato1 = st.selectbox("Forma de Contato 1", ["Whatsapp", "Ligação", "Visita", "E-mail"])
+        formaContato2 = st.selectbox("Forma de Contato 2", ["Whatsapp", "Ligação", "Visita", "E-mail"])
+        tipoContato = st.selectbox("Tipo de Contato", ["Ativo", "Receptivo"])
+        pedidoGerado = st.selectbox("Pedido Gerado", ["Sim", "Não"])
+        motivoDeclino = st.selectbox("Motivo de Declínio", ["Sem demanda", "Preço", "Sem estoque", "Crédito", "Logística", "Prospecção"])
+        observacao = st.text_area("Observação")
         submit_button = st.form_submit_button("Adicionar Produto")
 
         if submit_button:
             response = requests.post(
-                "https://meu-app-backend.onrender.com/products/",
+                "https://meu-app-backend.onrender.com/contacts/",
                 json={
-                    "name": name,
-                    "description": description,
-                    "price": price,
-                    "categoria": categoria,
-                    "email_fornecedor": email_fornecedor,
+                    "operador": operador,
+                    "dataContato": dataContato,
+                    "nomeCliente": nomeCliente,
+                    "pessoaContato": pessoaContato,
+                    "formaContato1": formaContato1,
+                    "formaContato2": formaContato2,
+                    "tipoContato": tipoContato,
+                    "pedidoGerado": pedidoGerado,
+                    "motivoDeclino": motivoDeclino,
+                    "observacao": observacao,
                 },
             )
             show_response_message(response)
-# Visualizar Produtos
-with st.expander("Visualizar Produtos"):
-    if st.button("Exibir Todos os Produtos"):
-        response = requests.get("https://meu-app-backend.onrender.com/products/")
+
+# Visualizar Contatos
+with st.expander("Visualizar Contatos"):
+    if st.button("Exibir Todos os Contatos"):
+        response = requests.get("https://meu-app-backend.onrender.com/contacts/")
         if response.status_code == 200:
-            product = response.json()
-            df = pd.DataFrame(product)
+            contact = response.json()
+            df = pd.DataFrame(contact)
 
             df = df[
                 [
                     "id",
-                    "name",
-                    "description",
-                    "price",
-                    "categoria",
-                    "email_fornecedor",
+                    "operador",
+                    "dataContato",
+                    "nomeCliente",
+                    "pessoaContato",
+                    "formaContato1",
+                    "formaContato2",
+                    "tipoContato",
+                    "pedidoGerado",
+                    "motivoDeclino",
+                    "observacao",
                     "created_at",
                 ]
             ]
@@ -78,23 +91,28 @@ with st.expander("Visualizar Produtos"):
         else:
             show_response_message(response)
 
-# Obter Detalhes de um Produto
-with st.expander("Obter Detalhes de um Produto"):
-    get_id = st.number_input("ID do Produto", min_value=1, format="%d")
-    if st.button("Buscar Produto"):
-        response = requests.get(f"https://meu-app-backend.onrender.com/products/{get_id}")
+# Obter Detalhes de um Contato
+with st.expander("Obter Detalhes de um Contato"):
+    get_id = st.number_input("ID do Contato", min_value=1, format="%d")
+    if st.button("Buscar Contato"):
+        response = requests.get(f"https://meu-app-backend.onrender.com/contacts/{get_id}")
         if response.status_code == 200:
-            product = response.json()
-            df = pd.DataFrame([product])
+            contact = response.json()
+            df = pd.DataFrame([contact])
 
             df = df[
                 [
                     "id",
-                    "name",
-                    "description",
-                    "price",
-                    "categoria",
-                    "email_fornecedor",
+                    "operador",
+                    "dataContato",
+                    "nomeCliente",
+                    "pessoaContato",
+                    "formaContato1",
+                    "formaContato2",
+                    "tipoContato",
+                    "pedidoGerado",
+                    "motivoDeclino",
+                    "observacao",
                     "created_at",
                 ]
             ]
@@ -104,48 +122,56 @@ with st.expander("Obter Detalhes de um Produto"):
         else:
             show_response_message(response)
 
-# Deletar Produto
-with st.expander("Deletar Produto"):
-    delete_id = st.number_input("ID do Produto para Deletar", min_value=1, format="%d")
-    if st.button("Deletar Produto"):
-        response = requests.delete(f"https://meu-app-backend.onrender.com/products/{delete_id}")
+# Deletar Contato
+with st.expander("Deletar Contato"):
+    delete_id = st.number_input("ID do Contato para Deletar", min_value=1, format="%d")
+    if st.button("Deletar Contato"):
+        response = requests.delete(f"https://meu-app-backend.onrender.com/contacts/{delete_id}")
         show_response_message(response)
 
-# Atualizar Produto
-with st.expander("Atualizar Produto"):
-    with st.form("update_product"):
-        update_id = st.number_input("ID do Produto", min_value=1, format="%d")
-        new_name = st.text_input("Novo Nome do Produto")
-        new_description = st.text_area("Nova Descrição do Produto")
-        new_price = st.number_input(
-            "Novo Preço",
-            min_value=0.01,
-            format="%f",
-        )
-        new_categoria = st.selectbox(
-            "Nova Categoria",
-            ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
-        )
-        new_email = st.text_input("Novo Email do Fornecedor")
+# Atualizar Contato
+with st.expander("Editar Contato"):
+    with st.form("update_contact"):
+        update_id = st.number_input("ID do Contato", min_value=1, format="%d")
+        new_operador = st.selectbox("Novo Operador", ["Matheus", "Lucas", "João", "Pedro", "Maria", "Ana", "José", "Carlos"])
+        new_dataContato = st.date_input("Nova Data do Contato")
+        new_nomeCliente = st.text_input("Novo Nome do Cliente")
+        new_pessoaContato = st.text_input("Nova Pessoa de Contato")
+        new_formaContato1 = st.selectbox("Nova Forma de Contato 1", ["Whatsapp", "Ligação", "Visita", "E-mail"])
+        new_formaContato2 = st.selectbox("Nova Forma de Contato 2", ["Whatsapp", "Ligação", "Visita", "E-mail"])
+        new_tipoContato = st.selectbox("Novo Tipo de Contato", ["Ativo", "Receptivo"])
+        new_pedidoGerado = st.selectbox("Novo Pedido Gerado", ["Sim", "Não"])
+        new_motivoDeclino = st.selectbox("Novo Motivo de Declínio", ["Sem demanda", "Preço", "Sem estoque", "Crédito", "Logística", "Prospecção"])
+        new_observacao = st.text_area("Nova Observação")
 
-        update_button = st.form_submit_button("Atualizar Produto")
+        update_button = st.form_submit_button("Atualizar Contato")
 
         if update_button:
             update_data = {}
-            if new_name:
-                update_data["name"] = new_name
-            if new_description:
-                update_data["description"] = new_description
-            if new_price > 0:
-                update_data["price"] = new_price
-            if new_email:
-                update_data["email_fornecedor"] = new_email
-            if new_categoria:
-                update_data["categoria"] = new_categoria
+            if new_operador:
+                update_data["operador"] = new_operador
+            if new_dataContato:
+                update_data["dataContato"] = new_dataContato
+            if new_nomeCliente:
+                update_data["nomeCliente"] = new_nomeCliente
+            if new_pessoaContato:
+                update_data["pessoaContato"] = new_pessoaContato
+            if new_formaContato1:
+                update_data["formaContato1"] = new_formaContato1
+            if new_formaContato2:
+                update_data["formaContato2"] = new_formaContato2
+            if new_tipoContato:
+                update_data["tipoContato"] = new_tipoContato
+            if new_pedidoGerado:
+                update_data["pedidoGerado"] = new_pedidoGerado
+            if new_motivoDeclino:
+                update_data["motivoDeclino"] = new_motivoDeclino
+            if new_observacao:
+                update_data["observacao"] = new_observacao
 
             if update_data:
                 response = requests.put(
-                    f"https://meu-app-backend.onrender.com/products/{update_id}", json=update_data
+                    f"https://meu-app-backend.onrender.com/contacts/{update_id}", json=update_data
                 )
                 show_response_message(response)
             else:
