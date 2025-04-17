5758221526e0f53b42c825a387704be64a981192
Renato Macedo <renatolmb@hotmail.com>
2025-04-16 22:58:27 -0300

    App pronto para deploy na Digital Ocean

--- frontend/app.py
+++ frontend/app.py
@@ -17,7 +17,8 @@ load_css("styles.css")
 st.image("logo.png", width=200)
 st.title("Gerenciamento de Contatos")
 
-path_backend = "https://backend-contatos.onrender.com/contacts/"
+# path_backend = "https://backend-contatos.onrender.com/contacts/"
+path_backend = "http://backend:8000/contacts/"
 
 
 # Função auxiliar para exibir mensagens de erro detalhadas
